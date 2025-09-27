ecdb9efdafff4cbc4a03a76ff06ef772e4159bf4
Ninel <ninelgruzdeva@gmail.com>
2025-09-28 01:32:11 +0500

    feat: add AppHeader

---
+++ components/AppHeader.tsx
@@ -0,0 +1,24 @@
+import { SettingsDialog } from './SettingsDialog';
+import { StyleSheet, Text, View } from 'react-native';
+import { FONT_FAMILY, GlobalStyles } from '../constants';
+
+export const AppHeader = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>File Converter</Text>
+      <SettingsDialog />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontFamily: FONT_FAMILY.regular,
+    fontSize: GlobalStyles.fontSize.title1,
+  },
+});
