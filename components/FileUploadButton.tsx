f62d366b383fafed8b4ad0694df080c7d1d2c44f
Ninel <ninelgruzdeva@gmail.com>
2025-09-28 00:27:23 +0500

    feat: add file upload btn

---
+++ components/FileUploadButton.tsx
@@ -0,0 +1,50 @@
+import { COLORS, GlobalStyles } from '../constants';
+import {
+  GestureResponderEvent,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { DocumentPickerAsset } from 'expo-document-picker';
+
+interface FileUploadButtonProps {
+  selectedFile: DocumentPickerAsset | null;
+  onFileUpload: (event: GestureResponderEvent) => void;
+}
+
+export const FileUploadButton = ({
+  selectedFile,
+  onFileUpload,
+}: FileUploadButtonProps) => {
+  return (
+    <Pressable
+      onPress={onFileUpload}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
+      <View style={styles.container}>
+        <FontAwesome name="upload" size={40} color={COLORS.primaryOrange} />
+        <Text>{selectedFile ? selectedFile.name : 'Upload File'}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.7,
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    padding: GlobalStyles.spacing.xs,
+    borderRadius: GlobalStyles.spacing.xs,
+    borderStyle: 'dashed',
+    borderWidth: 2,
+    borderColor: COLORS.grey3,
+    width: 200,
+    height: 200,
+  },
+});
